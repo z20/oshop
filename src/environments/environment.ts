efe42e0d47e64e94862b56367d331347594a3ff6
Zach Holdren <zholdren20@gmail.com>
2019-05-23 21:18:11 -0700

    Create app and add Firebase environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBW5441f6h6pXLzKvJcdmi0nHI04XSWOas",
+    authDomain: "oshop-b1d8a.firebaseapp.com",
+    databaseURL: "https://oshop-b1d8a.firebaseio.com",
+    projectId: "oshop-b1d8a",
+    storageBucket: "oshop-b1d8a.appspot.com",
+    messagingSenderId: "589368366783",
+    appId: "1:589368366783:web:65d2e5b120fc2dbb"
+  }
 };
 
 /*
