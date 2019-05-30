53d941f9eb0ddd3a00531ecf4dbbf63b409c0eca
Zach Holdren <zholdren20@gmail.com>
2019-05-30 15:34:07 -0700

    edit gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBW5441f6h6pXLzKvJcdmi0nHI04XSWOas",
+    authDomain: "oshop-b1d8a.firebaseapp.com",
+    databaseURL: "https://oshop-b1d8a.firebaseio.com",
+    projectId: "oshop-b1d8a",
+    storageBucket: "oshop-b1d8a.appspot.com",
+    messagingSenderId: "589368366783",
+    appId: "1:589368366783:web:636dc462d02b2a6f"
+  }
+};
